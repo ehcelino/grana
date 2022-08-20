8bbac3e310f79ce412d80643d4fa45f8d0174df7
Eduardo C <ehcelino@gmail.com>
2022-08-19 22:32:57 -0300

    Informação da licença no código-fonte

--- grafico.py
+++ grafico.py
@@ -3,6 +3,26 @@ from matplotlib import pyplot as plt
 import PySimpleGUI as sg
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+"""
+	Grana! Personal finance management system
+    Copyright (C) 2022  Eduardo C.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    <https://github.com/ehcelino/grana>
+"""
+
+
 arqdb = './db/grana.db'
 
 def mov_lista_cat_mensal(mesano):
